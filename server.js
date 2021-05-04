e7a160b070e277dae6fbc41b26fc91bf76d0c390
AlexRajya <55755818+AlexRajya@users.noreply.github.com>
2021-05-04 21:22:00 +0100

    Result saving added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-
 // Libraries
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const http = require('http');
 const ip = require('ip');
 
@@ -10,10 +10,21 @@ const port = process.env.PORT || 8080;
 const app = express();
 const server = http.createServer(app);
 
-// server functions
-
 app.use(express.static(`${__dirname}/public`));
 app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Save simulation data sent from client
+app.post('/save', (req, res) => {
+  try {
+    fs.writeFileSync('results.txt', JSON.stringify(req.body));
+    res.sendStatus(200);// OK
+    console.log('results saved');
+  } catch (err) {
+    res.sendStatus(400);// bad request
+  }
+});
 
 // start the server
 server.listen(port, () => {
